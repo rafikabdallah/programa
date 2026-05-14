19dc1882e03bda688f863150ef6af150ecdd2588
Abdallah Rafik <rafikabdallahumbb@gmail.com>
2026-05-14 10:37:48 +0200

    Polish demo build with guide, audio, ending and web fixes

--- scenes/main_menu_scripts/main_menu.gd
+++ scenes/main_menu_scripts/main_menu.gd
@@ -8,86 +8,85 @@ extends Control
 
 @onready var about_panel = $AboutPanel
 @onready var back_button = $AboutPanel/BackButton
+@onready var exit_message_label: Label = $ExitMessageLabel
 
+@onready var ambience_player: AudioStreamPlayer = $UISounds/AmbiencePlayer
 @onready var hover_sound: AudioStreamPlayer = $UISounds/HoverSound
 @onready var press_sound: AudioStreamPlayer = $UISounds/PressSound
 @onready var panel_open_sound: AudioStreamPlayer = $UISounds/PanelOpenSound
 @onready var panel_close_sound: AudioStreamPlayer = $UISounds/PanelCloseSound
-@onready var ambience_player: AudioStreamPlayer = $UISounds/AmbiencePlayer
 
 
 func _ready():
 	about_panel.visible = false
+	exit_message_label.visible = false
 
-	play_ambience()
+	ambience_player.play()
 
-	# Press signals
 	start_button.pressed.connect(_on_start_button_pressed)
 	about_button.pressed.connect(_on_about_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
 	back_button.pressed.connect(_on_back_button_pressed)
 
-	# Hover signals
-	start_button.mouse_entered.connect(_play_hover_sound)
-	about_button.mouse_entered.connect(_play_hover_sound)
-	exit_button.mouse_entered.connect(_play_hover_sound)
-	back_button.mouse_entered.connect(_play_hover_sound)
-
-
-func play_ambience():
-	if ambience_player != null:
-		ambience_player.play()
-
-
-func _play_hover_sound():
-	if hover_sound != null:
-		hover_sound.play()
-
-
-func _play_press_sound():
-	if press_sound != null:
-		press_sound.play()
-
-
-func _play_panel_open_sound():
-	if panel_open_sound != null:
-		panel_open_sound.play()
-
-
-func _play_panel_close_sound():
-	if panel_close_sound != null:
-		panel_close_sound.play()
+	start_button.mouse_entered.connect(play_hover_sound)
+	about_button.mouse_entered.connect(play_hover_sound)
+	exit_button.mouse_entered.connect(play_hover_sound)
+	back_button.mouse_entered.connect(play_hover_sound)
 
 
 func _on_start_button_pressed():
-	_play_press_sound()
-
+	play_press_sound()
 	await get_tree().create_timer(0.12).timeout
 	get_tree().change_scene_to_file(intro_scene_path)
 
 
 func _on_about_button_pressed():
-	_play_press_sound()
+	play_panel_open_sound()
+
+	exit_message_label.visible = false
 	about_panel.visible = true
-	_play_panel_open_sound()
 
 
 func _on_back_button_pressed():
-	_play_press_sound()
+	play_panel_close_sound()
+
 	about_panel.visible = false
-	_play_panel_close_sound()
 
 
 func _on_exit_button_pressed():
-	if OS.has_feature("web"):
-		show_web_exit_message()
-	else:
-		get_tree().quit()
-		
-		
-func show_web_exit_message():
-	about_panel.visible = true
+	play_press_sound()
 
-	var about_text = about_panel.get_node_or_null("AboutText")
-	if about_text != null:
-		about_text.text = "You can close the browser tab to exit."
+	if OS.has_feature("web"):
+		show_exit_message_temporarily()
+	else:
+		await get_tree().create_timer(0.12).timeout
+		get_tree().quit()
+
+
+func show_exit_message_temporarily():
+	about_panel.visible = false
+	exit_message_label.visible = true
+
+	await get_tree().create_timer(3.0).timeout
+
+	exit_message_label.visible = false
+
+
+func play_hover_sound():
+	hover_sound.stop()
+	hover_sound.play()
+
+
+func play_press_sound():
+	press_sound.stop()
+	press_sound.play()
+
+
+func play_panel_open_sound():
+	panel_open_sound.stop()
+	panel_open_sound.play()
+
+
+func play_panel_close_sound():
+	panel_close_sound.stop()
+	panel_close_sound.play()
